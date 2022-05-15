9b2de6244ba572f1d334dacd63bcf76f60ec8c62
felipe8919 <felipegomesrodrigues19@gmail.com>
2022-05-15 15:01:34 -0300

    inicia implementação de teste item produto compra

---
+++ Ph.Test/Entity/ItemProdutoCompraTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ph.Test.Entity
+{
+
+    public class ItemProdutoCompraTest
+    {
+        [Fact]
+        public void DeveSomarItensProdutoCompra()
+        {
+
+        }
+
+    }
+}
